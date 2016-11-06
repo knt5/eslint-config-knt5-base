9a1d9dfeac518aa7d3aeeb62d7b4a16c985494d6
knt5 <knt5@users.noreply.github.com>
2016-11-06 22:44:26 +0900

    Add a main module

---
+++ index.js
@@ -0,0 +1,20 @@
+module.exports = {
+	extends: [
+		'eslint-config-airbnb-base/rules/best-practices',
+		'eslint-config-airbnb-base/rules/errors',
+		'eslint-config-airbnb-base/rules/node',
+		'eslint-config-airbnb-base/rules/style',
+		'eslint-config-airbnb-base/rules/variables',
+		'eslint-config-airbnb-base/rules/es6',
+	],
+
+	rules: {
+		camelcase: 'error',
+		'func-names': 'off',
+		indent: ['error', 'tab'],
+		'no-console': 'off',
+		'no-plusplus': 'off',
+		'no-tabs': 'off',
+		'no-use-before-define': 'off',
+	},
+};
